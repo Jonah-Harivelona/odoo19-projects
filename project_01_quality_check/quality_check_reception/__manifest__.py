e1b56f1318fb21832633453bfc7eca1dfae9e9ce
Jonah Harivelona <jonahrakoto2001@gmail.com>
2026-06-29 09:06:29 +0200

    feat: vues XML quality check template

--- project_01_quality_check/quality_check_reception/__manifest__.py
+++ project_01_quality_check/quality_check_reception/__manifest__.py
@@ -5,11 +5,13 @@
     'description': "Gérer les contrôles de qualité lors de la réception des produits",
     'depends': ['stock', 'purchase', 'mail'],
     'data': [
+        'security/quality_check_groups.xml',
         'security/ir.model.access.csv',
-        #'security/quality_check_groups.xml',
         # 'security/quality_check_record_rules.xml',
         
-        'data/quality_check_sequence.xml'
+        'data/quality_check_sequence.xml',
+        
+        'views/quality_check_template_views.xml',
         
     ],
     'installable': True,
