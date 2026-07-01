e1e1845ab085e1a557a096c97be6b8ad488e011f
Jonah Harivelona <jonahrakoto2001@gmail.com>
2026-07-01 17:37:03 +0200

    feat: correction de manifest

--- project_01_quality_check/quality_check_reception/__manifest__.py
+++ project_01_quality_check/quality_check_reception/__manifest__.py
@@ -10,8 +10,10 @@
         # 'security/quality_check_record_rules.xml',
         
         'data/quality_check_sequence.xml',
-        
+        'views/quality_check_views.xml',
+        'views/quality_check_menus.xml',
         'views/quality_check_template_views.xml',
+        
         
     ],
     'installable': True,
